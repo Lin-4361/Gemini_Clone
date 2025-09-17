31ca4ba99722d5e947c94cd5f0462a096f971c5f
Ya Wei Lin <yaweilin2006@gmail.com>
2025-09-16 22:17:45 -0400

    No AI Yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-// src/App.jsx
 import React from 'react'
 import SideBar from './components/SideBar/SideBar'
 import Main from './components/Main/Main'
-import ContextProvider from './context/Context'
 
 const App = () => {
   return (
-    <ContextProvider>
+    <>
       <SideBar/>
       <Main/>
-    </ContextProvider>
+    </>
   )
 }
 
